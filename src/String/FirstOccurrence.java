c720eb75a36f7bf291b09410ef83227fbae490d6
PuShPaK-kUmAr <pushpak.prabhakar@anywhere.co>
2023-01-07 13:13:06 +0530

    First Occurenence problem added

--- src/String/FirstOccurrence.java
+++ src/String/FirstOccurrence.java
@@ -12,14 +12,13 @@ public class FirstOccurrence {
         System.out.print("The First Occurrence Index of Second String in First String is :"+res);
     }
 
-    private static int findOccurrence(String s1, String s2){
+    private static int findOccurrence(String s1, Strice ng s2){
         if(s1.length()==s2.length()) {
             return 0;
         }
         for(int i=0;i<(s1.length()-s2.length());i++){
             String k = s1.substring(i,i+s2.length());
             if(s2.equals(k)){                               // equals is used to compare to strings in java
-                System.out.println("Enter  :");
                 return i;
             }
         }
