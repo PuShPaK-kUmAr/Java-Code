8465be006872127f5a84d5f5d3d61447c6431af5
MANJEET KUMAR <manjeet.murari@anywhere.co>
2023-01-10 15:20:50 +0530

    Please follow the naming convention

--- src/Math/PrimeNumber.java
+++ src/Math/PrimeNumber.java
@@ -3,7 +3,7 @@ package Math;
 import java.util.Scanner;
 public class PrimeNumber {
     public static void main(String[] args){
-        try (Scanner sc = new Scanner(System.in)) {
+        try (Scanner sc = new Scanner(System.in)) {. // what sc means , what int a meeans??
             System.out.print("ENTER A NUMBER :");
             int a = sc.nextInt();
             checkPrime(a);
@@ -18,7 +18,7 @@ public class PrimeNumber {
         }else{
             for(i=2;i<=half;i++){
                 if(n%i==0){
-                    System.out.println(n+" is not a prime number");
+                    System.out.println(n+" is not a prime number");  // what in n means .?
                     flag = 1;
                     break;
                 }
