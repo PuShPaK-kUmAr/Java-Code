02e66ca212dc8191fee074cb6c7e9581229f79bd
PuShPaK-kUmAr <pushpak.prabhakar@anywhere.co>
2023-01-10 15:45:27 +0530

    naming convention fixed

--- src/Math/PrimeNumber.java
+++ src/Math/PrimeNumber.java
@@ -3,28 +3,28 @@ package Math;
 import java.util.Scanner;
 public class PrimeNumber {
     public static void main(String[] args){
-        try (Scanner sc = new Scanner(System.in)) {
+        try (Scanner userInput = new Scanner(System.in)) {
             System.out.print("ENTER A NUMBER :");
-            int a = sc.nextInt();
-            checkPrime(a);
+            int inputInteger = userInput.nextInt();
+            checkPrime(inputInteger);
         }
     }
 
-    static void checkPrime(int n){
-        int i, half, flag=0;
-        half= (n/2);
-        if(n==0 || n==1){
-            System.out.println(n+" is not a prime number");
+    static void checkPrime(int num){
+        int flag=0;
+        int halfOfNum= (num/2);
+        if(num==0 || num==1){
+            System.out.println(num+" is not a prime number");
         }else{
-            for(i=2;i<=half;i++){
-                if(n%i==0){
-                    System.out.println(n+" is not a prime number");
+            for(int count=2;count<=halfOfNum;count++){
+                if(num%count==0){
+                    System.out.println(num+" is not a prime number");
                     flag = 1;
                     break;
                 }
             }
             if(flag==0){
-                System.out.println(n+" is a prime number");
+                System.out.println(num+" is a prime number");
             }
         }
     }
