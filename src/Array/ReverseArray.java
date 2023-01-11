742353a500128e8262c06b8d5152f8e8033a2adb
PuShPaK-kUmAr <pushpak.prabhakar@anywhere.co>
2023-01-11 18:47:55 +0530

    reverse an array

---
+++ src/Array/ReverseArray.java
@@ -0,0 +1,31 @@
+package Array;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Enter the length of Array : ");
+        int arrayLength = userInput.nextInt();
+        int[] element = new int[arrayLength];
+        for (int count = 0; count < arrayLength; count++) {
+            System.out.print("Enter Element " + (count + 1) + " : ");
+            element[count] = userInput.nextInt();
+        }
+        System.out.print("The reversed Array is : " + reverse(element));
+    }
+
+    public static String reverse(int[] array) {
+        int left = 0;
+        int right = array.length - 1;
+        while (left < right) {
+            int temp = array[left];
+            array[left] = array[right];
+            array[right] = temp;
+            left++;
+            right--;
+        }
+        return Arrays.toString(array);
+    }
+}
