50a6db1003b8850ef8c6b3be08e8a3d07344bf68
PuShPaK-kUmAr <pushpak.prabhakar@anywhere.co>
2023-01-30 21:26:28 +0530

    pyramid paattern printing

--- src/Pattern/Pyramid.java
+++ src/Pattern/Pyramid.java
@@ -1,13 +1,18 @@
 package Pattern;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Pyramid {
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
         System.out.println("Enter the height of Pyramid as Integer : ");
-        int height = userInput.nextInt();
-        pyramid(height);
+        try {
+            int height = userInput.nextInt();
+            pyramid(height);
+        } catch (InputMismatchException error) {
+            System.out.println("Invalid input! Height must be an integer.");
+        }
     }
     private static void pyramid(int height) {
         for (int row = 0; row <= height; row++) {
